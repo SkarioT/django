c467d629b36eea67769c9028a063561b521150b9
SkarioT <vlad.skariot@gmail.com>
2020-06-10 21:59:56 +0300

    push to server

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/home/SkarioT/django/src/proj/static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT='/home/SkarioT/django/src/proj/media'
