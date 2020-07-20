fa940f58bb98788e79a7c733a0dde1910812ad5a
SkarioT <vlad.skariot@gmail.com>
2020-07-20 16:57:56 +0300

    -cours

--- src/home_page/views.py
+++ src/home_page/views.py
@@ -11,18 +11,20 @@ class Home_page(ListView):
     model=Books
     template_name='home_page/home_page.html'
     paginate_by=5
+    # context_object_name="object_list"
     
-    def get_context_data(self, **kwargs):
-        nbrb = requests.get('https://www.nbrb.by/api/exrates/rates?periodicity=0')
-        course = nbrb.json()
-        rate = {}
-        for c in course:
-            if c.get('Cur_Abbreviation') == 'USD':
-                rate['USD'] = c.get('Cur_OfficialRate') 
-            elif c.get('Cur_Abbreviation') == 'EUR':
-                rate['EUR'] = c.get('Cur_OfficialRate')
-            elif c.get('Cur_Abbreviation') == 'RUB':
-                rate['RUB'] = c.get('Cur_OfficialRate') 
-        context = super().get_context_data(**kwargs)
-        context = {'USD': rate.get('USD'), 'EUR': rate.get('EUR'), 'RUB': rate.get('RUB'), 'rate': rate}
-        return context
+    # def get_context_data(self, **kwargs):
+    #     nbrb = requests.get('https://www.nbrb.by/api/exrates/rates?periodicity=0')
+    #     course = nbrb.json()
+    #     rate = {}
+    #     for c in course:
+    #         if c.get('Cur_Abbreviation') == 'USD':
+    #             rate['USD'] = c.get('Cur_OfficialRate') 
+    #         elif c.get('Cur_Abbreviation') == 'EUR':
+    #             rate['EUR'] = c.get('Cur_OfficialRate')
+    #         elif c.get('Cur_Abbreviation') == 'RUB':
+    #             rate['RUB'] = c.get('Cur_OfficialRate') 
+    #     context = super().get_context_data(**kwargs)
+    #     context = {'USD': rate.get('USD'), 'EUR': rate.get('EUR'), 'RUB': rate.get('RUB'), 'rate': rate}
+    #     return context
+
